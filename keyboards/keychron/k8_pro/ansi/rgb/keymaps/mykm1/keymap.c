38d5baf90ddd6bbdb8105efef91dbafee8017d71
Luke Roberson <luke.roberson@colorado.edu>
2022-10-07 11:09:09 -0600

    added sleep button and enables n key rollover, which was not enabled for seom reason?

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/mykm1/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/mykm1/keymap.c
@@ -24,7 +24,7 @@ enum layers{
   MAC_FN,
   WIN_BASE,
   FN,
-  COM,
+  OPT,
   NUMPAD
 };
 
@@ -34,13 +34,16 @@ enum {
   TD_F8PL = 1,
   TD_F9SK = 2,
   TD_F10M = 3,
+  TD_F11D = 4,
+  TD_F12U = 5,
 };
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_F7PR] = ACTION_TAP_DANCE_DOUBLE(KC_F7, KC_MPRV),
   [TD_F8PL] = ACTION_TAP_DANCE_DOUBLE(KC_F8, KC_MPLY),
   [TD_F9SK] = ACTION_TAP_DANCE_DOUBLE(KC_F9, KC_MNXT),
   [TD_F10M] = ACTION_TAP_DANCE_DOUBLE(KC_F10, KC_MUTE),
-
+  [TD_F11D] = ACTION_TAP_DANCE_DOUBLE(KC_F11, KC_VOLD),
+  [TD_F12U] = ACTION_TAP_DANCE_DOUBLE(KC_F12, KC_VOLU),
 };
 
 
@@ -62,12 +65,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [WIN_BASE] = LAYOUT_ansi_87( 
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    TD(TD_F7PR),  TD(TD_F8PL),  TD(TD_F9SK),  TD(TD_F10M),  KC_F11,   KC_F12,                     KC_PSCR,  KC_CTANA, RGB_MOD,
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    TD(TD_F7PR),TD(TD_F8PL),TD(TD_F9SK),TD(TD_F10M),  TD(TD_F11D),    TD(TD_F12U),KC_PSCR,  KC_TRNS,  RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,           KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,          KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                    KC_ENT,              
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                    KC_RSFT,            KC_UP,    
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(COM),  MO(FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  LT(OPT,KC_APP),  MO(FN),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [FN] = LAYOUT_ansi_87( 
      KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            KC_TRNS,  KC_TRNS,  RGB_TOG,
@@ -75,15 +78,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,           
      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,   
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  TG(COM),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  TG(OPT),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
-[COM] = LAYOUT_ansi_87( 
+[OPT] = LAYOUT_ansi_87( 
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_7,    KC_8,     KC_9,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_4,    KC_5,     KC_6,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_DOT,   KC_0,     KC_1,    KC_2,     KC_3,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SLEP,  KC_TRNS,  KC_TRNS,  KC_4,    KC_5,     KC_6,
+     KC_TRNS,  NK_ON,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_DOT,   KC_0,     KC_1,    KC_2,     KC_3,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,           
      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,           KC_TRNS,   
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  OSL(FN),  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS)
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS)
     
 };
 
